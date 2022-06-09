3dbc0d2ad0a0340ae7345ec9f91253021bbba71f
Isabelly1 <isabellymartins846@gmail.com>
2022-06-09 16:06:46 -0300

    At1 JavaScript

---
+++ script.js
@@ -0,0 +1,11 @@
+let nome = `Isabelly`
+let idade = 19
+let ano = 2022
+let sub = ano - idade
+console.log(idade + ano)
+
+console.log("Aprendendo console.log")
+
+let quartaFeira = true
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
